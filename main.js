8e3b59984bf6715e631acb1e4cfaecb312005ce6
thehoat <mdoman@othot.com>
2017-03-06 16:10:16 -0500

    update hide

---
+++ main.js
@@ -0,0 +1,30 @@
+// main js
+
+function myFunction (){
+
+    $('#fb').hide();
+    $('#linkedin').hide();
+    $('#res').hide();
+    $('#email').hide();
+    $('#arrow').hide();
+
+
+    setTimeout(function(){
+        $('#arrow').fadeIn(3000);
+    },13500);
+    setTimeout(function(){
+        $('#linkedin').fadeIn(3000);
+    },19000);
+    setTimeout(function(){
+        $('#email').fadeIn(3000);
+    },19500);
+    setTimeout(function(){
+        $('#res').fadeIn(3000);
+    },20000);
+    setTimeout(function(){
+        $('#fb').fadeIn(3000);
+    },20500);
+
+
+
+}
